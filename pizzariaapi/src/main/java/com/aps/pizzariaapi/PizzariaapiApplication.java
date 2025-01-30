0c83fac97962a1749fa8ce62910d8a853a04b302
samleticias <sammyaleticia09@gmail.com>
2025-01-30 20:26:42 -0300

    first commit

---
+++ pizzariaapi/src/main/java/com/aps/pizzariaapi/PizzariaapiApplication.java
@@ -0,0 +1,13 @@
+package com.aps.pizzariaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaapiApplication.class, args);
+	}
+
+}
---
+++ pizzariaapi/src/test/java/com/aps/pizzariaapi/PizzariaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.pizzariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

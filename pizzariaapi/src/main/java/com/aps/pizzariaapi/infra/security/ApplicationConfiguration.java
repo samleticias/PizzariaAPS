2ace125d58276c5314fa6ea2c906b76464c74e87
samleticias <sammyaleticia09@gmail.com>
2025-02-02 11:49:48 -0300

    feat: adiciona página de listar pedidos cadastrados

--- pizzariaapi/src/main/java/com/aps/pizzariaapi/infra/security/ApplicationConfiguration.java
+++ pizzariaapi/src/main/java/com/aps/pizzariaapi/infra/security/ApplicationConfiguration.java
@@ -1,7 +1,5 @@
 package com.aps.pizzariaapi.infra.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
---
+++ pizzariafront/loginPage/index.js
@@ -0,0 +1,21 @@
+// document.getElementById("endereco-cep").addEventListener("input", function (e) {
+//     let cep = e.target.value.replace(/\D/g, "").slice(0, 8);
+//     if (cep.length > 5) {
+//         cep = `${cep.slice(0, 5)}-${cep.slice(5)}`;
+//     }
+//     e.target.value = cep;
+// });
+
+// document.getElementById("cliente-telefone").addEventListener("input", function (e) {
+//     let tel = e.target.value.replace(/\D/g, "").slice(0, 11);
+//     if (tel.length > 10) {
+//         tel = `(${tel.slice(0, 2)}) ${tel.slice(2, 7)}-${tel.slice(7)}`;
+//     } else if (tel.length > 6) {
+//         tel = `(${tel.slice(0, 2)}) ${tel.slice(2, 6)}-${tel.slice(6)}`;
+//     } else if (tel.length > 2) {
+//         tel = `(${tel.slice(0, 2)}) ${tel.slice(2)}`;
+//     } else if (tel.length > 0) {
+//         tel = `(${tel}`;
+//     }
+//     e.target.value = tel;
+// });
